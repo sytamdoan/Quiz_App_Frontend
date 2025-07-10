c569a87ba3f267f5d4f37285db6fb630dace9213
Tam Doan <sytamdoan@gmail.com>
2025-07-09 21:06:08 -0500

    Added frontend component for quizzes

---
+++ src/services/QuizServices.js
@@ -0,0 +1,16 @@
+import apiClient from "./services";
+
+export default {
+  getQuiz(classID) {
+    return apiClient.get("/Class/" + classID + "/Quiz/");
+  },
+  addQuiz(classID, Quiz) {
+    return apiClient.post("/Class/" + classID + "/Quiz/", Quiz);
+  },
+  updateQuiz(QuizID, Quiz) {
+    return apiClient.put("/Quiz/" + QuizID, Quiz);
+  },
+  deleteQuiz(QuizID) {
+    return apiClient.delete("/Quiz/" + QuizID)
+  }
+};
