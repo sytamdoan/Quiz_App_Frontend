a1d49fc2bbb736b076ef9726f2d0e4b9de0fe89e
Tam Doan <sytamdoan@gmail.com>
2025-07-21 17:44:44 -0500

    Added wait page for students after submitting question

--- src/views/StudentQuestion.vue
+++ src/views/StudentQuestion.vue
@@ -2,12 +2,21 @@
 import { onMounted } from 'vue'
 import { io } from 'socket.io-client';
 import { ref, computed  } from "vue";
+import { useRouter } from "vue-router";
 
 const socket = io('http://localhost:3001');
+const router = useRouter();
 const quizSessionID = ref(1);
 const currentQuestion = ref("");
 const answerSet = ref({})
 const selectedAnswer = ref();
+const waitingForNextQuestion = ref(true);
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+
 
 onMounted(async () => {
   try {
@@ -17,9 +26,12 @@ onMounted(async () => {
     socket.on(quizSessionID.value + "question", (data) => {
       currentQuestion.value = data;
     })
-
     socket.on(quizSessionID.value + "answer", (data) => {
       answerSet.value = data;
+    })
+    socket.on(quizSessionID.value + "nextQuestion", (data) => {
+      waitingForNextQuestion.value = false;
+      selectedAnswer.value = null;
     })
 
   } catch (error) {
@@ -27,18 +39,67 @@ onMounted(async () => {
   }
 });
 
+function closeSnackBar() {
+    snackbar.value.value = false;
+}
+
+function openConfirmationBar() {
+    if(selectedAnswer.value) {
+        snackbar.value.color = "red";
+        snackbar.value.text = "Submit This Answer?";
+        snackbar.value.value = true;
+    } else {
+        snackbar.value.color = "red";
+        snackbar.value.text = "Please Select An Answer";
+        snackbar.value.value = true;
+    }
+}
+
+function submitAnswer() {
+    snackbar.value.value = false;
+    socket.emit("response", {
+        quizSessionID: quizSessionID.value,
+        answer: selectedAnswer.value,
+    });
+    selectedAnswer.value = null;
+    waitingForNextQuestion.value = true;
+}
 </script>
 
 <template>
-    <h1 style="text-align: center;">{{ currentQuestion }}</h1>
+    <h1 style="text-align: center;" v-if="!waitingForNextQuestion">{{ currentQuestion }}</h1>
+    <h1 style="text-align: center;" v-if="waitingForNextQuestion">Please Wait for Instructor</h1>
 
-    <v-radio-group v-model = "selectedAnswer" >
+    <v-radio-group v-model = "selectedAnswer" v-if="!waitingForNextQuestion">
         <v-radio
         v-for="answer in answerSet"
         :key="answer.id"
         :label="answer.answerText"
-        :value="answer.id"
+        :value="answer"
         />
     </v-radio-group>
 
+    <v-card-actions>
+        <v-btn variant="flat" color="primary" @click="openConfirmationBar()" v-if="!waitingForNextQuestion">Submit</v-btn>
+    </v-card-actions>
+
+    <v-snackbar v-model="snackbar.value" rounded="pill">
+      {{ snackbar.text }}
+      <template v-slot:actions>
+        <v-btn
+          variant="text"
+          @click="closeSnackBar()"
+        >
+          Close
+        </v-btn>
+        <v-btn
+          v-if="selectedAnswer"
+          :color="snackbar.color"
+          variant="text"
+          @click="submitAnswer()"
+        >
+          Submit
+        </v-btn>
+      </template>
+    </v-snackbar>
 </template>
