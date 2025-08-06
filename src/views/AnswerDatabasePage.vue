a0ac0a6a564f666e4576b950b9188e06e74bc020
An nguyen <annguyen3132@gmail.com>
2025-08-06 17:15:56 -0500

    hide/show isCorrect column when type is quiz. Also change appearance of the isCorrect column

--- src/views/AnswerDatabasePage.vue
+++ src/views/AnswerDatabasePage.vue
@@ -143,16 +143,19 @@ function closeSnackBar() {
     <thead>
       <tr>
         <th class="text-left">ID</th>
+        <th v-if="quizType=='quiz'" class="text-left">Is Correct</th>
         <th class="text-left">Answer Text</th>
-        <th class="text-left">Is Correct</th>
         <th class="text-left">Actions</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="Item in filteredData" :key="Item.id" class="mb-2">
         <td class = "cursor-pointer" @click="openUpdateModal(Item, false)">{{ Item.id }}</td>
+        <td v-if="quizType=='quiz'" class = "cursor-pointer" @click="openUpdateModal(Item, false)">
+          <v-icon v-if="Item.isCorrect" color="green">mdi-check-circle</v-icon>
+          <v-icon v-else color="red">mdi-close-circle</v-icon>
+        </td>
         <td class = "cursor-pointer" @click="openUpdateModal(Item, false)">{{ Item.answerText }}</td>
-        <td class = "cursor-pointer" @click="openUpdateModal(Item, false)">{{ Item.isCorrect }}</td>
         <td v-if="isEditable">
           <v-icon color="red" class="cursor-pointer" @click="openUpdateModal(Item, false)"> mdi-pencil </v-icon>
           |
@@ -179,7 +182,7 @@ function closeSnackBar() {
           required
         ></v-text-field>
 
-        <v-radio-group
+        <v-radio-group v-if="quizType=='quiz'"
           v-model="selectedItem.isCorrect"
           label="Correct Answer?"
           required
