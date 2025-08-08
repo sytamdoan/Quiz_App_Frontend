9e3cefc9b666ab37c77511cb53163077ec16a4f7
Tam Doan <sytamdoan@gmail.com>
2025-08-07 22:07:37 -0500

    testing

--- src/views/ProfessorQuestion.vue
+++ src/views/ProfessorQuestion.vue
@@ -85,6 +85,8 @@ const chartSettings = computed(() => ({
 
 
 onMounted(async () => {
+  console.log("Address");
+  console.log(`${import.meta.env.VITE_DB_HOST}`);
   try {
     quizSessionID.value = route.params.quizSessionID;
     await grabQuizSession();
